d96f296ab11e9ebd9581238b60c168dfa6e5a4f5
MartinAguilarR <martin.aguilarr@utem.cl>
2021-05-19 12:52:25 +0200

    Actualización

--- Tokenizado NLTK.py
+++ Tokenizado NLTK.py
@@ -1,8 +1,20 @@
-import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
+from collections import Counter
+from collections import OrderedDict
 
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "í"),
+        ("ó", "o"),
+        ("ú", "u")
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+        return s
 
 with open('out_text.txt', 'r') as miarchivo:
     texto = miarchivo.read()
@@ -15,8 +27,23 @@ word_tokens = word_tokenize(texto)
 word_tokens = list(filter(lambda token: token not in string.punctuation, word_tokens))
 filtro = []
 
+# ciclo para revisar las palabras que no están en las stopwords
 for palabra in word_tokens:
-    if palabra is not stopwords:
+    if palabra not in stop_words:
         filtro.append(palabra)
 
 print(word_tokens)
+print(type(filtro))
+
+
+# Contabilizar top n° palabras tras stopwords
+c = Counter(filtro)
+print(c.most_common(10))
+
+#Orden de las mas repetidas
+y = OrderedDict(c.most_common())
+
+with open('revision.txt', 'w') as f:
+    for k, v in y.items():
+        f.write(f'palabra:{k} n°:{v}\n') # Crear nueva linea por elemento; formato lista
+
