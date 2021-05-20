2717bf686fa7b70863b8f1cca586fe26ff05a8c8
MartinAguilarR <martin.aguilarr@utem.cl>
2021-05-20 13:40:17 +0200

    actualización

--- Tokenizado NLTK.py
+++ Tokenizado NLTK.py
@@ -1,49 +1,74 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import re
 import string
 from collections import Counter
 from collections import OrderedDict
 
 
-def normalize(s):
-    replacements = (
-        ("á", "a"),
-        ("é", "e"),
-        ("í", "í"),
-        ("ó", "o"),
-        ("ú", "u")
-    )
-    for a, b in replacements:
-        s = s.replace(a, b).replace(a.upper(), b.upper())
-        return s
-
+'''
+Paso 1: Se abre el archivo de texto y se le genera una limpieza base;
+transformación a minúsculas, eliminación de espacios en blanco, números, puntuación 
+y elementos web.
+'''
 
 with open('out_text.txt', 'r') as miarchivo:
     archivo = miarchivo.read()
-    texto = archivo.lower()
+    # Se transforma el texto en minúscula
+    nuevo_texto = archivo.lower()
+    # Eliminación de páginas web (palabras que empiezan por "http")
+    nuevo_texto = re.sub('http\S+', ' ', nuevo_texto)
+    # Eliminación de signos de puntuación
+    regex = '[\\n°\\n\\!\\"\\#\\$\\%\\&\\\'\\(\\)\\*\\+\\,\\-\\.\\/\\:\\;\\<\\=\\>\\?\\@\\[\\\\\\]\\^_\\`\\{\\|\\}\\~\\”\\“]'
+    nuevo_texto = re.sub(regex, ' ', nuevo_texto)
+    # Eliminación de números
+    nuevo_texto = re.sub("\d+", ' ', nuevo_texto)
+    # Eliminación de espacios en blanco multiples
+    nuevo_texto = re.sub("\\s+", ' ', nuevo_texto)
+    print(len(nuevo_texto))
 
+# Tokenizamos en Español
 stop_words = set(stopwords.words('spanish'))
-word_tokens = word_tokenize(texto)
+tokens = word_tokenize(nuevo_texto)
+print(tokens)
 
 # Aplicamos una funcion para encontrar elementos que no estén en puntuación
-word_tokens = list(filter(lambda token: token not in string.punctuation, word_tokens))
+tokens_clean = list(filter(lambda token: token not in string.punctuation, tokens))
 filtro = []
 
 # Ciclo para revisar las palabras que no están en las stopwords
-for palabra in word_tokens:
+for palabra in tokens_clean:
     if palabra not in stop_words:
         filtro.append(palabra)
 
-print(word_tokens)
-print(type(filtro))
+print(len(filtro))
+print(filtro)
 
+tagged = nltk.pos_tag(filtro)
+print(tagged)
+
+'''
+Paso 2: Se pasa a contar las palabras mas repetidas, para luego buscar,
+detalladamente, las veces que aparece la palabra en un contexto.
+'''
 # Contabilizar top n° palabras tras stopwords
 c = Counter(filtro)
-print(c.most_common(50))
+print(c.most_common(15))
 
 # Orden de las mas repetidas
-y = OrderedDict(c.most_common())
+y = OrderedDict(c.most_common(50))
 
 with open('revision.txt', 'w') as f:
     for k, v in y.items():
-        f.write(f'palabra:{k} n°:{v}\n')  # Crear nueva linea por elemento; formato lista
+        f.write(f'palabra:{k} n°:{v}\n')  # Genera documento en formato lista
+
+tagged_c = nltk.pos_tag(y)
+print(tagged_c)
+
+# Match de palabra con oración
+text = nltk.Text(word_tokenize(nuevo_texto))
+match = text.concordance('comision') # Se cambia la palabra
